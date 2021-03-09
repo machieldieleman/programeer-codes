77c37206ad639b732eb2e8cd0b32f66cd6dc983d
machieldieleman <machieldieleman@icloud.com>
2021-03-09 13:22:39 +0100

    updated

--- cursussen/inloggen.php
+++ cursussen/inloggen.php
@@ -73,3 +73,15 @@ if (isset($_GET['ingelogd'])) {
     </table> 
 </form>      
 </body>
+<?php
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
